7e0407979c7815164847d76b7e6c874f9847151f
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2025-06-11 10:31:11 +0200

    feat: deploy solana oft on devnet

--- programs/endpoint-mock/src/lib.rs
+++ programs/endpoint-mock/src/lib.rs
@@ -5,7 +5,7 @@ use anchor_lang::prelude::*;
 use instructions::*;
 use state::*;
 
-declare_id!("6xmPjYnXyxz36xcKkv2zCrZc72LK5hQ9xzY3EjeZ59MV");
+declare_id!("7zVAhU4dsmgLx43VHi5vj4nbjVhAHN8XkhvameNEG7xF");
 
 pub const OAPP_SEED: &[u8] = b"OApp";
 
